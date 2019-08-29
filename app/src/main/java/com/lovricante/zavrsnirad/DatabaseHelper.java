227b8675422a8859a511b4059b99de44607c7326
Ante Lovric <ante.lovric77@gmail.com>
2019-08-29 23:04:20 +0200

    Work on doc, change order of databaseHelper methods

--- app/src/main/java/com/lovricante/zavrsnirad/DatabaseHelper.java
+++ app/src/main/java/com/lovricante/zavrsnirad/DatabaseHelper.java
@@ -12,22 +12,29 @@ import com.google.android.gms.maps.model.LatLng;
 import java.util.ArrayList;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "ActivitiesData.db";
-    public static final String TABLE_NAME_ACTIVITIES = "Activities";
-    public static final String ACTIVITY_ID = "ActivityID";
-    public static final String ACTIVITY_TYPE = "Type";
-    public static final String DURATION = "Duration";
-    public static final String DISTANCE = "Distance";
+    private static final String DATABASE_NAME = "ActivitiesData.db";
+    private static final String TABLE_NAME_ACTIVITIES = "Activities";
+    private static final String ACTIVITY_ID = "ActivityID";
+    private static final String ACTIVITY_TYPE = "Type";
+    private static final String DURATION = "Duration";
+    private static final String DISTANCE = "Distance";
 
-    public static final String TABLE_NAME_POSITION_DATA = "PositionData";
-    public static final String POSITION_DATA_ID = "PositionDataID";
-    public static final String LATITUDE = "Latitude";
-    public static final String LONGITUDE = "Longitude";
+    private static final String TABLE_NAME_POSITION_DATA = "PositionData";
+    private static final String POSITION_DATA_ID = "PositionDataID";
+    private static final String LATITUDE = "Latitude";
+    private static final String LONGITUDE = "Longitude";
+    private static final String TIMESTAMP = "Timestamp";
 
-    public static final String TIMESTAMP = "Timestamp";
-
-    public DatabaseHelper(@Nullable Context context) {
+    DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 2);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_ACTIVITIES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_POSITION_DATA);
+
+        onCreate(db);
     }
 
     @Override
@@ -45,14 +52,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 LONGITUDE + " REAL NOT NULL, " +
                 TIMESTAMP + " LONG NOT NULL, " +
                 ACTIVITY_ID + " INTEGER NOT NULL, " +
-                "FOREIGN KEY(" + ACTIVITY_ID + ") REFERENCES " + TABLE_NAME_ACTIVITIES + "(" + ACTIVITY_ID + "));");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_ACTIVITIES);
-
-        onCreate(db);
+                "FOREIGN KEY(" + ACTIVITY_ID + ") REFERENCES " +
+                TABLE_NAME_ACTIVITIES + "(" + ACTIVITY_ID + "));");
     }
 
     public void insertActivity(ActivityData data) {
@@ -73,6 +74,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             int activityId = cursor.getInt(0);
 
             insertPositionData(activityId, data.getTimePlaces());
+        }
+    }
+
+    private void insertPositionData(int activityId, ArrayList<TimePlace> data) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        for (TimePlace it : data) {
+            db.execSQL("INSERT INTO " + TABLE_NAME_POSITION_DATA + " (" +
+                    ACTIVITY_ID + ", " + LATITUDE + ", " + LONGITUDE + ", " +
+                    TIMESTAMP + ") VALUES (" +
+                    activityId + ", " +
+                    it.getLatitude() + ", " +
+                    it.getLongitude() + ", " +
+                    it.getTime() + ");");
         }
     }
 
@@ -114,19 +129,5 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
 
         return timePlaces;
-    }
-
-    private void insertPositionData(int activityId, ArrayList<TimePlace> data) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        for (TimePlace it : data) {
-            db.execSQL("INSERT INTO " + TABLE_NAME_POSITION_DATA + " (" +
-                    ACTIVITY_ID + ", " + LATITUDE + ", " + LONGITUDE + ", " +
-                    TIMESTAMP + ") VALUES (" +
-                    activityId + ", " +
-                    it.getLatitude() + ", " +
-                    it.getLongitude() + ", " +
-                    it.getTime() + ");");
-        }
     }
 }
